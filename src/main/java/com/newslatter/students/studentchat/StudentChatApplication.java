0c3d0bc87c6f92dcedbb9783585677f6b3f51dbc
Alexander Iliev <azxsasho@gmail.com>
2019-01-09 14:17:07 +0200

    Initial commit

---
+++ src/main/java/com/newslatter/students/studentchat/StudentChatApplication.java
@@ -0,0 +1,14 @@
+package com.newslatter.students.studentchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentChatApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/newslatter/students/studentchat/StudentChatApplicationTests.java
@@ -0,0 +1,17 @@
+package com.newslatter.students.studentchat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentChatApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
